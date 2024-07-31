3897b2629ca5aab68da6f753bfceec9e70680f03
notnotmelon <zacharypicco@gmail.com>
2024-07-31 15:20:03 -0500

    feat: Improved support for calling conventions

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,15 @@ use std::os::raw::c_double;
 use rivets::detour;
 use tracing::info;
 
-#[repr(C)] struct MapPosition;
-#[repr(C)] struct ForceID;
-#[repr(C)] struct EntityWithHealth;
+#[repr(C)]
+struct MapPosition;
+#[repr(C)]
+struct ForceID;
+#[repr(C)]
+struct EntityWithHealth;
 
 #[detour(?findRandomTarget@Surface@@QEAAPEAVEntityWithHealth@@VMapPosition@@VForceID@@NAEBV?$function@$$A6A_NAEBVEntityWithHealth@@@Z@std@@@Z)]
-extern "C" fn find_random_target(_: MapPosition, _: ForceID, _: c_double, _: ()) -> *const EntityWithHealth {
+fn find_random_target(_: MapPosition, _: ForceID, _: c_double, _: ()) -> *const EntityWithHealth {
     info!("Detoured into Surface::findRandomTarget!");
     1 as *const EntityWithHealth
 }
