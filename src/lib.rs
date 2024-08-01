e5daa6223fa4aba2de24a400e4e50887789fdf05
notnotmelon <zacharypicco@gmail.com>
2024-07-31 23:39:30 -0500

    e

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,11 @@ struct MapPosition;
 struct ForceID;
 #[repr(C)]
 struct EntityWithHealth;
+#[repr(C)]
+struct Surface;
 
 #[detour(?findRandomTarget@Surface@@QEAAPEAVEntityWithHealth@@VMapPosition@@VForceID@@NAEBV?$function@$$A6A_NAEBVEntityWithHealth@@@Z@std@@@Z)]
-fn find_random_target(_: MapPosition, _: ForceID, _: c_double, _: ()) -> *const EntityWithHealth {
+fn find_random_target(this: Surface, _: MapPosition, _: ForceID, _: c_double, _: ()) -> *const EntityWithHealth {
     info!("Detoured into Surface::findRandomTarget!");
     1 as *const EntityWithHealth
 }
