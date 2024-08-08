fbe517a25474a778cddccd111e9d7b66c796f4df
notnotmelon <zacharypicco@gmail.com>
2024-08-08 13:22:17 -0500

    feat: Implement TryFrom<usize> for defines macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::ffi::c_void;
-
 use rivets::defines;
 use rivets::detour;
 use rivets::Opaque;
@@ -8,17 +6,20 @@ use tracing::info;
 #[detour(?run@LuaEventDispatcher@@AEAAXW4LuaEventType@@VMapTickType@@P8LuaGameScript@@EAA_NAEBVGameAction@@@Z2@Z)]
 fn run(
     this: Opaque,
-    lua_event_type: i32,
+    lua_event_type: usize,
     map_tick_type: Opaque,
     lua_game_script: Opaque,
     game_action: Opaque,
 ) {
-    if lua_event_type == 0 {
-        return unsafe { back(this, lua_event_type, map_tick_type, lua_game_script, game_action); }
-    }
-    let q: Result<defines::events, _> = (lua_event_type as u8).try_into();
-    if let Ok(q) = q {
-        info!("Rust event handler! {:?}", q);
+    let event = defines::events::try_from(&lua_event_type);
+    match event {
+        Ok(defines::events::on_tick) => {},
+        Ok(_) => {
+            info!("Rust event handler! {:?}", event);
+        },
+        Err(_) => {
+            info!("Rust event handler! {:?}", event);
+        }
     }
     unsafe { back(this, lua_event_type, map_tick_type, lua_game_script, game_action) }
 }
