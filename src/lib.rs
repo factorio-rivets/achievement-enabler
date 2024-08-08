5d66cbde105d7c1929a2a0efaf3a61fc6d2bb01a
notnotmelon <zacharypicco@gmail.com>
2024-08-07 23:39:10 -0500

    feat: Implement from try_into trait to convert u8,u16 ect into defines entries

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
 use std::ffi::c_void;
 
+use rivets::defines;
 use rivets::detour;
 use tracing::info;
 
 #[detour(?run@LuaEventDispatcher@@AEAAXW4LuaEventType@@VMapTickType@@P8LuaGameScript@@EAA_NAEBVGameAction@@@Z2@Z)]
-fn run(this: *const c_void, lua_event_type: i32, map_tick_type: *const c_void, lua_game_script: *const c_void, game_action: *const c_void) {
+fn run(
+    this: *const c_void,
+    lua_event_type: i32,
+    map_tick_type: *const c_void,
+    lua_game_script: *const c_void,
+    game_action: *const c_void,
+) {
     if lua_event_type == 0 {
-        unsafe { source(this, lua_event_type, map_tick_type, lua_game_script, game_action); }
+        unsafe {
+            source(
+                this,
+                lua_event_type,
+                map_tick_type,
+                lua_game_script,
+                game_action,
+            );
+        }
         return;
     }
     info!("Blocked event? {:?}", lua_event_type);
-    unsafe { source(this, lua_event_type, map_tick_type, lua_game_script, game_action); }
+    let q: defines::events = (lua_event_type as u8).try_into().unwrap();
+    info!("Blocked event? {:?}", q);
+    unsafe {
+        source(
+            this,
+            lua_event_type,
+            map_tick_type,
+            lua_game_script,
+            game_action,
+        );
+    }
 }
