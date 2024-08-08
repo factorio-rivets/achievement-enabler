6a7b1dbdb5b1d837a4c1db7d99d17c4227a2c519
notnotmelon <zacharypicco@gmail.com>
2024-08-08 10:29:19 -0500

    feat: Added the Opaque type for whenever you would like to skip writing FFI for C++ types

--- src/lib.rs
+++ src/lib.rs
@@ -2,38 +2,23 @@ use std::ffi::c_void;
 
 use rivets::defines;
 use rivets::detour;
+use rivets::Opaque;
 use tracing::info;
 
 #[detour(?run@LuaEventDispatcher@@AEAAXW4LuaEventType@@VMapTickType@@P8LuaGameScript@@EAA_NAEBVGameAction@@@Z2@Z)]
 fn run(
-    this: *const c_void,
+    this: Opaque,
     lua_event_type: i32,
-    map_tick_type: *const c_void,
-    lua_game_script: *const c_void,
-    game_action: *const c_void,
+    map_tick_type: Opaque,
+    lua_game_script: Opaque,
+    game_action: Opaque,
 ) {
     if lua_event_type == 0 {
-        unsafe {
-            source(
-                this,
-                lua_event_type,
-                map_tick_type,
-                lua_game_script,
-                game_action,
-            );
-        }
-        return;
+        return unsafe { back(this, lua_event_type, map_tick_type, lua_game_script, game_action); }
     }
-    info!("Blocked event? {:?}", lua_event_type);
-    let q: defines::events = (lua_event_type as u8).try_into().unwrap();
-    info!("Blocked event? {:?}", q);
-    unsafe {
-        source(
-            this,
-            lua_event_type,
-            map_tick_type,
-            lua_game_script,
-            game_action,
-        );
+    let q: Result<defines::events, _> = (lua_event_type as u8).try_into();
+    if let Ok(q) = q {
+        info!("Rust event handler! {:?}", q);
     }
+    unsafe { back(this, lua_event_type, map_tick_type, lua_game_script, game_action) }
 }
