0962154eb039eb2168b831ac6d1848f27530369f
notnotmelon <zacharypicco@gmail.com>
2024-08-08 21:13:47 -0500

    feat: Allow infinite detours in a single mod

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,28 @@ fn run(
 ) {
     let event = defines::events::try_from(&lua_event_type);
     match event {
-        Ok(defines::events::on_tick) => {},
-        Ok(_) => {
-            info!("Rust event handler! {:?}", event);
-        },
-        Err(_) => {
+        Ok(defines::events::on_tick) => {}
+        Ok(event) => {
             info!("Rust event handler! {:?}", event);
         }
+        Err(_) => {
+            info!("Unknown event {:?}", lua_event_type);
+        }
     }
-    unsafe { back(this, lua_event_type, map_tick_type, lua_game_script, game_action) }
+    unsafe {
+        back(
+            this,
+            lua_event_type,
+            map_tick_type,
+            lua_game_script,
+            game_action,
+        );
+    }
 }
+
+#[detour(?valid@LuaSurface@@UEBA_NXZ)]
+const fn valid(this: Opaque) -> bool {
+    false
+}
+
+rivets::initialize!();
